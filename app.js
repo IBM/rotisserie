e62a47e1ad3e5aad321afc4a609995d3b61127bc
Cullen Taylor <cullentaylor@outlook.com>
2017-07-18 15:20:39 -0500

    Add basic Twtich auth
    * Add twitch api initialization, still need to get user access token.
    
    Signed-off-by: Cullen Taylor <cullentaylor@outlook.com>

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const express = require('express')
-const app = express()
+#!/usr/bin/env node
+
+const express = require('express');
+const fs = require('fs');
+const app = express();
 
 function main() {
+  //auth with twitch
+  var TwitchApi = require('twitch-api');
+  var twitch = new TwitchApi({
+      clientId: process.env.client_id,
+      clientSecret: process.env.client_secret,
+      redirectUri: 'localhost',
+      scopes: '']
+  });
+
+  //serve index.html
   app.get('*', function (req, res) {
     res.sendFile(__dirname + '/public/index.html')
-  })
+  });
 
+  //start http server and log success
   app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
-  })
+  });
 }
 
 main();
