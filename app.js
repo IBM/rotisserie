10767cca762a19ea0dad83cff241e934f901a010
Cullen Taylor <cullentaylor@outlook.com>
2017-07-19 15:37:32 -0500

    Add listStreams function
    * listStreams is a function that will asynchronously fetch a list of
      PUBG streams from twitch.
    
    Signed-off-by: Cullen Taylor <cullentaylor@outlook.com>

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 #!/usr/bin/env node
 
+// TODO: determine logging strategy.
+
 const express = require('express');
 const fs = require('fs');
 const app = express();
 
+function listStreams(twitch, callback) {
+  twitch.getStreams([game="PLAYERUNKNOWN\'/S BATTLEGROUNDS"],
+  function(err, body){
+    if (err){
+      console.log(err);
+    }
+    else {
+      return callback(body);
+    }
+  });
+}
+
 function main() {
-  //auth with twitch
+  // init client and auth with Twitch
   var TwitchApi = require('twitch-api');
   var twitch = new TwitchApi({
       clientId: process.env.client_id,
@@ -14,6 +28,7 @@ function main() {
       response_type: 'code'
   });
 
+  /* TODO: determine if obtaining an access token is required. most likely not.
   var access_token = "";
   twitch.getAccessToken(twitch.code, function(err, body){
     if (err){
@@ -23,9 +38,16 @@ function main() {
       access_token = body.access_token;
     }
   });
+  */
 
-  console.log(access_token);
+  var streams = "";
+  listStreams(twitch, function(response) {
+    streams = response;
+    console.log(streams);
+    // TODO: pass list of streams to recording function here.
+  });
 
+  /* TODO: uncomment and serve when writing to index.html is ready.
   //serve index.html
   app.get('*', function (req, res) {
     res.sendFile(__dirname + '/public/index.html')
@@ -35,6 +57,7 @@ function main() {
   app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
   });
+  */
 }
 
 main();
