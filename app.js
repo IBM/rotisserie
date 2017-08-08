47a56519cf30f2820487b64608a00dcd577ec104
Cullen Taylor <cullentaylor@outlook.com>
2017-08-08 15:45:38 -0500

    Switch to workerpool model for stream processing
    * Add workerpool to dependencies.
    * Create a workerpool in app.js, where each worker will take a stream name
      from listStreams and perform the actions of recording video, taking a
      single frame and cropping that frame for a given string.
    * OCR is still done as a batch process in order to more easily facilitate
      the array operations (anything passed to a worker must be stringifiable).
    * Improves performance by taking total processing runtime from 40s down to
      12s.
    * Tested on macOS Sierra and Ubuntu 16.04
    
    Signed-off-by: Cullen Taylor <cullentaylor@outlook.com>

--- app.js
+++ app.js
@@ -6,12 +6,10 @@ const path = require("path");
 
 // external dependencies
 const express = require("express");
-const FFMpeg = require("fluent-ffmpeg");
-const gm = require("gm").subClass({imageMagick: true});
 const Handlebars = require("handlebars");
-const {spawn} = require("child_process");
-const twitch = require("twitch-api-v5");
 const tesseract = require("node-tesseract");
+const twitch = require("twitch-api-v5");
+const workerpool = require("workerpool");
 
 // construct express app
 const app = express();
@@ -53,99 +51,12 @@ function listStreams(twitch, callback) {
 }
 
 /**
- * Records short clip of each stream gathered in listStreams.
- * @callback {string} - log message indicating all streams have a clip
- * recorded.
- * @param {object} streamsList - JSON object containing list of streams
- * and their associated metadata.
- * @param {string} clipsDir - Relative path to directory containing short
- * recorded clips of each stream in streamsList.
- * @param {requestCallback} callback - The callback that handles the response.
- * @return {requestCallback} callback - The callback that sends the log
- * message.
- */
-function recordStreams(streamsList, clipsDir, callback) {
-  for (let stream in streamsList.streams) {
-    let streamName = streamsList.streams[stream].channel.display_name;
-    console.log("recording clip of stream: " + streamName);
-
-    const child = spawn("livestreamer", ["-Q", "-f", "twitch.tv/" + streamName,
-      "720p", "-o", clipsDir + streamName + ".mp4"]);
-    setTimeout(function() {
-      child.kill("SIGINT");
-    }, 20000);
-  }
-  return callback("recorded all streams");
-}
-
-/**
- * Takes screenshots of all clips recorded in recordStreams.
- * @callback {string} - log message indicating all clips have had a screenshot
- * taken from them.
- * @param {object} streamsList - JSON object containing list of streams
- * and their associated metadata.
- * @param {string} clipsDir - Relative path to directory containing short
- * recorded clips of each stream in streamsList.
- * @param {string} thumbnailsDir - Relative path to directory containing
- * screenshots of each clip recorded in recordStreams.
- * @param {requestCallback} callback - The callback that handles the response.
- * @return {requestCallback} callback - The callback that sends the log
- * message.
- */
-function takeScreenshots(streamsList, clipsDir, thumbnailsDir, callback) {
-  for (let stream in streamsList.streams) {
-    let streamName = streamsList.streams[stream].channel.display_name;
-
-    if (fs.existsSync(clipsDir + streamName + ".mp4")) {
-      console.log("taking screenshot of stream: " + streamName);
-      new FFMpeg(clipsDir + streamName + ".mp4").takeScreenshots({
-        count: 1,
-        folder: thumbnailsDir,
-        filename: streamName + ".png",
-      });
-    }
-  }
-  return callback("took screenshots all streams");
-}
-
-/**
- * Crops all screenshots taken in takeScreenshots to just the area containing
- * the number of players alive in-game.
- * @callback {string} - log message indicating all screenshots have been
- * cropped.
- * @param {object} streamsList - JSON object containing list of streams
- * and their associated metadata.
- * @param {string} thumbnailsDir - Relative path to directory containing
- * screenshots of each clip recorded in recordStreams.
- * @param {string} cropsDir - Relative path to directory containing cropped
- * versions of all screenshots taken in takeScreenshots.
- * @param {requestCallback} callback - The callback that handles the response.
- * @return {requestCallback} callback - The callback that sends the log
- * message.
- */
-function cropScreenshots(streamsList, thumbnailsDir, cropsDir, callback) {
-  for (let stream in streamsList.streams) {
-    let streamName = streamsList.streams[stream].channel.display_name;
-    console.log("cropping screenshot of stream: " + streamName);
-
-    if (fs.existsSync(thumbnailsDir + streamName + ".png")) {
-      gm(thumbnailsDir + streamName + ".png")
-        .crop(28, 20, 1190, 25)
-        .write(cropsDir + streamName + ".png", function(err) {
-          if (err) console.log(err);
-        });
-    }
-  }
-  return callback("cropped all screenshots");
-}
-
-/**
  * Uses Tesseract OCR software to interpret the number in each cropped
  * screenshot created in cropScreenshots.
  * @callback {object} - object containing the name of the stream and its
  * associated number of players alive.
  * @param {string} cropsDir - Relative path to directory containing cropped
- * versions of all screenshots taken in takeScreenshots.
+ * versions of all screenshots taken in takeScreenshot.
  * @param {string} file - filename of cropped screenshot to interpret. Gained
  * from readdirSync call in runner.
  * @param {requestCallback} callback - The callback that handles the response.
@@ -155,6 +66,7 @@ function interpretCrop(cropsDir, file, callback) {
     psm: 8,
   };
 
+  console.log("interpreting: " + __dirname + cropsDir.replace(".", "") + file);
   tesseract.process(__dirname + cropsDir.replace(".", "") + file, options,
     function(err, text) {
       let object = {};
@@ -198,6 +110,7 @@ function main() {
   const clipsDir = "./streams/clips/";
   const thumbnailsDir = "./streams/thumbnails/";
   const cropsDir = "./streams/crops/";
+  let pool = workerpool.pool(__dirname + "/worker.js");
 
   // auth with Twitch
   twitch.clientID = process.env.client_id;
@@ -210,39 +123,34 @@ function main() {
   listStreams(twitch, function(response) {
     let streamsList = response;
     let array = [];
-    recordStreams(streamsList, clipsDir, function(response) {
-      console.log(response);
-      setTimeout(function() {
-        takeScreenshots(streamsList, clipsDir, thumbnailsDir,
-          function(response) {
-            console.log(response);
-          });
-      }, 21000);
 
-      setTimeout(function() {
-        cropScreenshots(streamsList, thumbnailsDir, cropsDir,
-          function(response) {
-            console.log(response);
-          });
-      }, 24000);
-
-      setTimeout(function() {
-        fs.readdirSync(cropsDir).forEach((file) => {
-          interpretCrop(cropsDir, file, function(response) {
-            array.push(response);
-          });
+    for (let stream in streamsList.streams) {
+      let streamName = streamsList.streams[stream].channel.display_name;
+      pool.exec("processStream", [streamName])
+        .catch(function(err) {
+          console.log(err);
+        })
+        .then(function(response) {
+          // pool.clear(false);
         });
-      }, 27000);
+    }
 
-      setTimeout(function() {
-        array.sort(function(a, b) {
-          return a.alive - b.alive;
+    setTimeout(function() {
+      fs.readdirSync(cropsDir).forEach((file) => {
+        interpretCrop(cropsDir, file, function(response) {
+          array.push(response);
         });
-        console.log(array);
-        console.log("lowest stream: " + array[0].name);
-        writeIndex(array[0].name);
-      }, 29000);
-    });
+      });
+    }, 10000);
+
+    setTimeout(function() {
+      array.sort(function(a, b) {
+        return a.alive - b.alive;
+      });
+      console.log(array);
+      console.log("lowest stream: " + array[0].name);
+      writeIndex(array[0].name);
+    }, 12000);
   });
 
   // serve index.html
