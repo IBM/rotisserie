00fe2bc505be00613c2d7d134f54aa2fd83b59a6
Spencer Krum <nibz@spencerkrum.com>
2017-08-09 16:00:29 -0500

    Improve UI
    * Don't reload stream if this streamer is still 'on top'
    * Put the name of the streamer in the HTML

--- public/js/pubgredzone.js
+++ public/js/pubgredzone.js
@@ -1,14 +1,14 @@
 
  $( document ).ready( function () {
-    var current_stream = ""
     updateIframe();
     setInterval(updateIframe, 15000);
     function updateIframe() {
       $.getJSON( "/current", function ( data ) {
-      console.log(current_stream);
+          var current_stream =  $("#twitch_iframe").prop('src');
+          console.log(current_stream);
           if (current_stream != data['stream_url']) {
             $("#twitch_iframe").prop('src', data['stream_url']);
-            current_stream = data['scream_url']
+            $("#streamer_name").text(data['stream_name']);
           }
       });
     }
