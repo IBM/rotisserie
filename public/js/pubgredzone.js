26965c0f03adae8e824d42fe7f1ee5612d3c6638
Spencer Krum <nibz@spencerkrum.com>
2017-08-09 17:52:14 -0500

    WIP: Stay with a player that is near the end
    If the streamer is below 15 players left alive, stay with them until the
    end. This is still broken because it will never switch.

--- public/js/pubgredzone.js
+++ public/js/pubgredzone.js
@@ -3,15 +3,17 @@ $(document).ready(function() {
   setInterval(updateIframe, 15000);
 
   /**
-   * Updates webpage on a 15s interval if a new best stream is determined.
+   * Updates webpage on a 15s interval if a new best stream is determined and there are more than 15 players alive on the current stream
    */
   function updateIframe() {
     $.getJSON("/current", function(data) {
       let currentStream = $("#twitch_iframe").prop("src");
       console.log(currentStream);
-      if (currentStream != data["stream_url"]) {
-        $("#twitch_iframe").prop("src", data["stream_url"]);
-        $("#streamer_name").text(data["stream_name"] + " - " + data["alive"]);
+      if (data['alive'] > 15) {
+        if (currentStream != data["stream_url"]) {
+          $("#twitch_iframe").prop("src", data["stream_url"]);
+          $("#streamer_name").text(data["stream_name"] + " - " + data["alive"]);
+        }
       }
     });
   }
