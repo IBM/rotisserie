f9510f1ba82fce492bc41c082a4be8e3064b563b
Cullen Taylor <cullentaylor@outlook.com>
2017-08-12 10:46:41 -0700

    Eslint cleanup.
    Signed-off-by: Cullen Taylor <cullentaylor@outlook.com>

--- ocrsvc.js
+++ ocrsvc.js
@@ -1,29 +1,30 @@
-
 // dependencies
 const express = require("express");
 const tesseract = require("node-tesseract");
 const multer = require("multer");
-var path = require('path');
-var uuid = require('uuid');
-var upload = multer(); // for parsing multipart/form-data
-var tmpdir = require('os').tmpdir(); // let the os take care of removing zombie tmp files
-var fs = require('fs');
+const path = require("path");
+const uuid = require("uuid");
+const upload = multer(); // for parsing multipart/form-data
+
+// let the os take care of removing zombie tmp files
+const tmpdir = require("os").tmpdir();
+const fs = require("fs");
 const app = express();
 
-app.get('/info', function (req, res) {
+app.get("/info", function(req, res) {
   result = {
     "app": "ocrsvc",
     "version": "0.1",
-    "health": "good"
-  }
+    "health": "good",
+  };
   res.json(result);
 });
 
-app.post('/process', upload.single('image'), function (req, res, next) {
+app.post("/process", upload.single("image"), function(req, res, next) {
   console.log(req.file);
-  var output = path.resolve(tmpdir, 'ocr-svc-' + uuid.v4());
+  let output = path.resolve(tmpdir, "ocr-svc-" + uuid.v4());
   fs.writeFile(output, req.file.buffer, function(err) {
-    if(err) {
+    if (err) {
       return console.log(err);
     }
     console.log("The file was saved as " + output);
@@ -38,11 +39,11 @@ app.post('/process', upload.single('image'), function (req, res, next) {
   });
 });
 
-app.post('/process_pubg', upload.single('image'), function (req, res, next) {
+app.post("/process_pubg", upload.single("image"), function(req, res, next) {
   console.log(req.file);
-  var output = path.resolve(tmpdir, 'ocr-svc-' + uuid.v4());
+  let output = path.resolve(tmpdir, "ocr-svc-" + uuid.v4());
   fs.writeFile(output, req.file.buffer, function(err) {
-    if(err) {
+    if (err) {
       return console.log(err);
     }
     console.log("The file was saved as " + output);
@@ -51,13 +52,13 @@ app.post('/process_pubg', upload.single('image'), function (req, res, next) {
     psm: 8,
   };
   tesseract.process(output, options, function(err, text) {
-    var number = parseFloat(text.trim());
-    if ( isNaN(number) ) {
+    let number = parseFloat(text.trim());
+    if (isNaN(number)) {
       number = 100;
     }
-    var result = {
+    let result = {
       "number": number,
-    }
+    };
     console.log(result);
     res.json(result);
   });
