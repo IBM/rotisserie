ea69e3b14fd567381af2fd9929f9e08de7cc64e3
Spencer Krum <nibz@spencerkrum.com>
2017-08-21 11:52:54 -0500

    Refactor
    This removes the worker pool entirely. The application is not cpu bound,
    we spend most of our time waiting for an http request or external
    program (ffmpeg, livestreamer, etc) so the worker pool doesn't gain us
    anything.
    
    This uses promises to chain together the four async tasks formerly
    in worker.js. This removes the need for timeouts and should make the application
    considerably more robust.

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const path = require("path");
 const express = require("express");
 const request = require("request");
 const twitch = require("twitch-api-v5");
-const workerpool = require("workerpool");
+
+const FFMpeg = require("fluent-ffmpeg");
+const gm = require("gm").subClass({imageMagick: true});
+const {spawn} = require("child_process");
 
 // construct express app
 const app = express();
@@ -64,13 +67,121 @@ function listStreams(twitch, callback) {
 }
 
 /**
+ * Records short clip of each stream gathered in listStreams.
+ * @param {object} options - object of other params
+ * @param {string} streamName - name of stream to record.
+ * @param {string} clipsDir - Relative path to directory containing short
+ * recorded clips of each stream in streamsList.
+ */
+function recordStream(options) {
+  return new Promise ((resolve, reject) => {
+    console.log("recording clip of stream: " + options.streamName);
+    const child = spawn("livestreamer", ["-Q", "-f", "--twitch-oauth-token",
+      process.env.token, "twitch.tv/" + options.streamName,
+      "720p", "-o", options.clipsDir + options.streamName + ".mp4"]);
+    setTimeout(function() {
+      child.kill("SIGINT");
+      console.log("recorded stream: " + options.streamName);
+      resolve(options);
+    }, 4000);
+  });
+}
+
+/**
+ * Takes screenshots of all clips recorded in recordStreams.
+ * @param {object} options - object of other params
+ * @param {string} streamName - name of stream's clip to screenshot.
+ * @param {string} clipsDir - Relative path to directory containing short
+ * recorded clips of each stream in streamsList.
+ * @param {string} thumbnailsDir - Relative path to directory containing
+ * screenshots of each clip recorded in recordStreams.
+ * @param {requestCallback} callback - The callback that handles the response.
+ */
+function takeScreenshot(options) {
+  return new Promise ((resolve, reject) => {
+    if (fs.existsSync(options.clipsDir + options.streamName + ".mp4")) {
+      console.log("taking screenshot of stream: " + options.streamName);
+      new FFMpeg(options.clipsDir + options.streamName + ".mp4").takeScreenshots({
+        count: 1,
+        folder: options.thumbnailsDir,
+        filename: options.streamName + ".png",
+      }).on('end', function() {
+        resolve(options)
+      });
+    }
+  });
+}
+
+/**
+ * Crops all screenshots taken in takeScreenshot to just the area containing
+ * the number of players alive in-game.
+ * @param {object} options - object of other params
+ * @param {string} streamName - name of stream's screenshot to crop.
+ * @param {string} thumbnailsDir - Relative path to directory containing
+ * screenshots of each clip recorded in recordStream.
+ * @param {string} cropsDir - Relative path to directory containing cropped
+ * versions of all screenshots taken in takeScreenshot.
+ */
+
+function cropScreenshot(options) {
+  return new Promise ((resolve, reject) => {
+  console.log("cropping screenshot of stream: " + options.streamName);
+    if (fs.existsSync(options.thumbnailsDir + options.streamName + ".png")) {
+      gm(options.thumbnailsDir + options.streamName + ".png")
+        .crop(28, 20, 1190, 25)
+        .write(options.cropsDir + options.streamName + ".png", function(err) {
+          resolve(options);
+          if (err) reject(err);
+        });
+      console.log("cropped screenshot of: " + options.streamName);
+    } else {
+      reject(streamName + ": input file not found");
+    }
+  });
+}
+
+
+
+/**
+ * OCR the data (via web request)
+ * Uses the pubgredzone-ocr microservice
+ *
+ */
+
+function ocrCroppedShot(options) {
+  return new Promise ((resolve, reject) => {
+    let formData = {
+      image: fs.createReadStream(__dirname
+                                 + options.cropsDir.replace(".", "") + options.streamName + ".png"),
+    };
+
+    let requestOptions = {
+      url: "http://localhost:3001/process_pubg",
+      formData: formData,
+    };
+
+    request.post(requestOptions, function(err, httpResponse, body) {
+      if (err) {
+        console.error("upload failed");
+        reject(err);
+      }
+      let parsed = JSON.parse(body);
+      let object = {};
+      object.name = options.streamName;
+      object.alive = parsed.number;
+      resolve(object);
+    });
+  });
+}
+
+/**
  * Runner for listing streams and firing up a worker for each of those streams
  * to handle the stream processing.
  * @param {object} pool - pool of workers to offload stream processing tasks on.
  * @param {string} cropsDir - path to directory containing cropped thumbnails
  * containing the number of players alive.
  */
-function updateStreamsList(pool, cropsDir) {
+function updateStreamsList(cropsDir) {
   // get list of twitch streams and record each one
   listStreams(twitch, function(response) {
     let streamsList = response;
@@ -79,39 +190,21 @@ function updateStreamsList(pool, cropsDir) {
 
     for (let stream in streamsList) {
       let streamName = streamsList[stream].channel.display_name;
-      pool.exec("processStream", [streamName])
-        .catch(function(err) {
-          console.log(err);
-        })
-        .then(function(response) {
-          // pool.clear(false);
+      var data = {
+        streamName: streamName,
+        clipsDir: "./streams/clips/",
+        thumbnailsDir: "./streams/thumbnails/",
+        cropsDir:"./streams/crops/",
+      }
+
+      recordStream(data)
+        .then(takeScreenshot)
+        .then(cropScreenshot)
+        .then(ocrCroppedShot)
+        .then(function(streamobj){
+          array.push(streamobj);
         });
     }
-
-    setTimeout(function() {
-      fs.readdirSync(cropsDir).forEach((file) => {
-        let formData = {
-          image: fs.createReadStream(__dirname
-                                     + cropsDir.replace(".", "") + file),
-        };
-
-        let requestOptions = {
-          url: "http://localhost:3001/process_pubg",
-          formData: formData,
-        };
-
-        request.post(requestOptions, function(err, httpResponse, body) {
-          if (err) {
-            return console.error("upload failed");
-          }
-          let parsed = JSON.parse(body);
-          let object = {};
-          object.name = file.replace(".png", "");
-          object.alive = parsed.number;
-          array.push(object);
-        });
-      });
-    }, 10000);
 
     setTimeout(function() {
       array.sort(function(a, b) {
@@ -155,7 +248,6 @@ function main() {
   const clipsDir = "./streams/clips/";
   const thumbnailsDir = "./streams/thumbnails/";
   const cropsDir = "./streams/crops/";
-  let pool = workerpool.pool(__dirname + "/worker.js");
 
   // auth with Twitch
   twitch.clientID = process.env.token;
@@ -165,11 +257,11 @@ function main() {
   ensureDir(cropsDir);
 
   // init website with lowest stream.
-  updateStreamsList(pool, cropsDir);
+  updateStreamsList(cropsDir);
 
   // continue searching for lowest stream every 15 seconds.
   setInterval(function() {
-    updateStreamsList(pool, cropsDir);
+    updateStreamsList(cropsDir);
   }, 15000);
 
   // serve index.html
